4868a67c4a63f75b30cd9d2197daac8baa4d18ad
kokekui <kokekui17@gmail.com>
2019-02-11 23:37:56 +0100

    Ejemplos de todo

--- public_html/index.js
+++ public_html/index.js
@@ -2,12 +2,12 @@
 // El boton reset, seria borrar las imagenes que contiene, y cargar
 // las imagenes del array en el div inicial
 //let imagenes = [document.getElementById("img1"),document.getElementById("img2"),document.getElementById("img3"),document.getElementById("img4"),];
-window.onload = function () {
-    let imagenes = [document.getElementById("img1"), document.getElementById("img2"),
-        document.getElementById("img3"), document.getElementById("img4")];
-    console.log(imagenes);
-    console.log(document.getElementById("img1"));
-}
+/*window.onload = function () {
+ var imagenes = [document.getElementById("img1"), document.getElementById("img2"),
+ document.getElementById("img3"), document.getElementById("img4")];
+ console.log(imagenes);
+ console.log(document.getElementById("img1"));
+ }*/
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -27,16 +27,30 @@ function drop(ev, divId) {
     }
 }
 //Plantear las imagenes como un array, para hacer el reset
-/*function reset() {
- location.reload();
- }*/
-
-function reset(ev) {
-    ev.preventDefault();
-    console.log(ev.dataTransfer.getData());
-    var data = ev.dataTransfer.getData("text");
-    for (var i = 0, max = 4; i < max; i++) {
-        imagenes.push(document.getElementById(data));
-        console.log(document.getElementById(data));
+function reset() {
+    var imagenes = [document.getElementById("img1"), document.getElementById("img2"),
+        document.getElementById("img3"), document.getElementById("img4")];
+    //console.log(document.getElementById("containerDerecha"));
+    //console.log(document.getElementById("drag1"));
+    //var imagen1 = document.getElementById("img1");
+    for (var i = 1, max = 5; i < max; i++) {
+        if (document.getElementById("drag" + i).hasChildNodes()) {//Recorre todos, y hay que comprobar que no tenga hijos
+            document.getElementById("drag" + i).removeChild(document.getElementById("img" + i));
+        }
+    }
+    for (var i = 1, max = 5; i < max; i++) {
+        if (!document.getElementById("container" + i).hasChildNodes()) {
+            document.getElementById("container" + i).appendChild(imagenes[i - 1]);
+        }
+    }
+    container = document.getElementById("animacion");
+    if ($(container).hasClass("c-animacion__container--inactivo")) {
+        $(container).removeClass("c-animacion__container--inactivo");
+        $(container).addClass("c-animacion__container");
+        setTimeout(function () {
+            $(container).removeClass("c-animacion__container");
+        }, 4000);
+        $(container).addClass("c-animacion__container--inactivo");
+        console.log(container);
     }
 }
